0a5b0d8b4ffefe52ac29283f54aa42363b14b047
leodidier14 <leo.didier@viacesi.fr>
2019-09-24 10:55:22 +0200

    mvc structure

--- A3ProjetMadMax/src/main/Main.java
+++ A3ProjetMadMax/src/main/Main.java
@@ -4,25 +4,15 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import view.FilesView;
+
 public class Main {
 
 	public static void main(String[] args) {
-		   JFrame window = new JFrame();
-		   window.setVisible(true);
-		   window.setSize(500, 500);
-		   window.setLocationRelativeTo(null);
-		   window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		   
+		   FilesView viewFiles =  new FilesView();
 		   
-		   JPanel panel = new JPanel();
-		   panel.setLayout(null);
-		   window.setContentPane(panel);
-		   JButton buttonSource = new JButton("Source File");
-		   buttonSource.setBounds(190, 180, 120, 25);
-		   panel.add(buttonSource);
-		   JButton buttonDestination = new JButton("Destination File");
-		   buttonDestination.setBounds(190, 225, 120, 25);
-		   panel.add(buttonDestination);
+		 
 
 	}
 
--- A3ProjetMadMax/src/view/MainFrame.java
+++ A3ProjetMadMax/src/view/MainFrame.java
@@ -4,4 +4,12 @@ import javax.swing.JFrame;
 
 public class MainFrame {
 	
+	 protected JFrame window = new JFrame();
+	 
+	public MainFrame() {
+		this.window.setVisible(true);
+		this.window.setSize(500, 500);
+		this.window.setLocationRelativeTo(null);
+		this.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
