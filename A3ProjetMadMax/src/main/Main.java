aa3def876df37b515afc9f5f9c736d2fa53b44ba
leodidier14 <leo.didier@viacesi.fr>
2019-09-24 10:08:37 +0200

    main commit

--- A3ProjetMadMax/src/main/Main.java
+++ A3ProjetMadMax/src/main/Main.java
@@ -7,7 +7,6 @@ import javax.swing.JPanel;
 public class Main {
 
 	public static void main(String[] args) {
-		
 		   JFrame window = new JFrame();
 		   window.setVisible(true);
 		   window.setSize(500, 500);
