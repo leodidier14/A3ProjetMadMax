55a1801ccd87a5cc92b33b8f915c0544313404e3
leodidier14 <leo.didier@viacesi.fr>
2019-09-24 09:59:46 +0200

    main frame+panel+button commit

---
+++ A3ProjetMadMax/src/main/main.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class main {
+
+	public static void main(String[] args) {
+		   JFrame window = new JFrame();
+		   window.setVisible(true);
+		   window.setSize(500, 500);
+		   window.setLocationRelativeTo(null);
+		   window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		   
+		   
+		   JPanel panel = new JPanel();
+		   panel.setLayout(null);
+		   window.setContentPane(panel);
+		   JButton buttonSource = new JButton("Source File");
+		   buttonSource.setBounds(190, 180, 120, 25);
+		   panel.add(buttonSource);
+		   JButton buttonDestination = new JButton("Destination File");
+		   buttonDestination.setBounds(190, 225, 120, 25);
+		   panel.add(buttonDestination);
+		   
+		    
+	}
+
+}
