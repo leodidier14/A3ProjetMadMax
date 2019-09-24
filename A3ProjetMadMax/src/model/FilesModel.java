5d3c2830d62ab4faf6cff808070c50b96c380eba
leodidier14 <leo.didier@viacesi.fr>
2019-09-24 14:07:37 +0200

    error input stream /

--- A3ProjetMadMax/src/model/FilesModel.java
+++ A3ProjetMadMax/src/model/FilesModel.java
@@ -1,12 +1,12 @@
 package model;
 
+import java.io.InputStream;
 import javax.swing.JFileChooser;
-
 import controller.FilesController;
 
 public class FilesModel {
 	
-	private FilesController filesController;
+	//private FilesController filesController;
 	
 	public void browseFiles() {
 		JFileChooser ChoixSource = new JFileChooser();
@@ -16,6 +16,20 @@ public class FilesModel {
                ChoixSource.getSelectedFile().
                      getAbsolutePath();
            }
+    }
+	
+	public String lireFicher(String path) throws Exception
+	{   
+		InputStream stream;
+		stream = new InputStream(path);
+        StringBuilder reponse;
+        reponse = new StringBuilder();
+        for(int a = stream.read();a!=-1;a=stream.read())
+        {
+        	reponse.append((char)a);
+        	}
+        stream.close();
+        return reponse.toString();
     }
 	
 	/*public void setController(FilesController filesController) {
