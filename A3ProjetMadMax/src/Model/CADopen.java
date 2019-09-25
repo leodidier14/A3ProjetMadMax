ba479152af2dc201c13cbbb8fdee439c9b5de026
leodidier14 <leo.didier@viacesi.fr>
2019-09-25 18:03:14 +0200

    AUTH LEO CONFIG

--- A3ProjetMadMax/src/Model/CADopen.java
+++ A3ProjetMadMax/src/Model/CADopen.java
@@ -1,102 +1,100 @@
-package Model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-public class CADopen {
-	
-	private static CADopen INSTANCE= null;
-    private String url = "jdbc:mysql://localhost/good?useSSL=false&serverTimezone=UTC";
-    private String username = "root";
-    private String password = "";
-    private Connection connection = null;
-    private Statement statement;
-    private ResultSet rs;
     
-	public CADopen() {
-		this.open();
-	}
-	
-	public static synchronized CADopen getInstance() {
-		if (CADopen.INSTANCE == null) {
-			CADopen.INSTANCE = new CADopen();
-		}
-		return CADopen.INSTANCE;
-	}
-	
-    
-    private void open() {
-        try {
-            Class.forName("com.mysql.jdbc.driver");
-            connection = DriverManager.getConnection(this.getUrl(), this.getUsername(), this.getPassword());
-        } catch (Exception e) {
-            e.printStackTrace();
+    package Model;
+
+    import java.sql.*;
+
+    public class CADopen {
+    	
+    	private static CADopen INSTANCE= null;
+    	private String url = "jdbc:mysql://localhost/projetseminaire?useSSL=false&serverTimezone=UTC";
+        private String username = "root";
+        private String password = "";
+        private Connection connection = null;
+        private Statement statement;
+        private ResultSet rs;
+        
+    	public CADopen() {
+    		this.open();
+    	}
+    	
+    	public static synchronized CADopen getInstance() {
+    		if (CADopen.INSTANCE == null) {
+    			CADopen.INSTANCE = new CADopen();
+    		}
+    		return CADopen.INSTANCE;
+    	}
+    	
+        
+        private void open() {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                connection = DriverManager.getConnection(this.getUrl(), this.getUsername(), this.getPassword());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-    }
 
-    public int getResultSize(String req_sql) {
-        int size = 0;
-        try {
-            Statement state = this.connection.createStatement();
-            ResultSet res = state.executeQuery(req_sql);
-            size = res.getFetchSize();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+        public int getResultSize(String req_sql) {
+            int size = 0;
+            try {
+                Statement state = this.connection.createStatement();
+                ResultSet res = state.executeQuery(req_sql);
+                res.last();
+                size = res.getRow();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return size;
         }
-        return size;
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public Connection getConnection() {
+            return connection;
+        }
+
+        public void setConnection(Connection connection) {
+            this.connection = connection;
+        }
+
+        public Statement getStatement() {
+            return statement;
+        }
+
+        public void setStatement(Statement statement) {
+            this.statement = statement;
+        }
+
+        public ResultSet getRs() {
+            return rs;
+        }
+
+        public void setRs(ResultSet rs) {
+            this.rs = rs;
+        }
+
+
     }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Connection getConnection() {
-        return connection;
-    }
-
-    public void setConnection(Connection connection) {
-        this.connection = connection;
-    }
-
-    public Statement getStatement() {
-        return statement;
-    }
-
-    public void setStatement(Statement statement) {
-        this.statement = statement;
-    }
-
-    public ResultSet getRs() {
-        return rs;
-    }
-
-    public void setRs(ResultSet rs) {
-        this.rs = rs;
-    }
-
-
-}
